2237c34b0783cb0320b1a9ae2265cc8249d7ac11
Nalithdev <Ethan.delcroix@outlook.fr>
2023-04-05 08:34:13 +0200

    appear destroyer and cruisser

---
+++ js/film.js
@@ -0,0 +1,10 @@
+
+
+gsap.to(".mc75", {x : 325, y : -250 , duration :  10 , scale: 3.5})
+gsap.to(".mc75" , {y : -245  , duration : 3 , delay : 10 , repeat :-1 , repeatDelay : 2})
+gsap.to(".mc75" ,   { y : -250  , duration : 2 , delay : 11 , repeat :-1 , repeatDelay : 3 })
+
+
+gsap.to(".destroyer", {x : -325, y : 250 , duration :  10 , scale: 3})
+gsap.to(".destroyer" , {y : 245  , duration : 3 , delay : 10 , repeat :-1 , repeatDelay : 2})
+gsap.to(".destroyer" ,   { y : 250  , duration : 2 , delay : 11 , repeat :-1 , repeatDelay : 3 })
