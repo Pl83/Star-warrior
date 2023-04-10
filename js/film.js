adf103787ff4bf608e241f1c7426cbdd947fb785
Nalithdev <Ethan.delcroix@outlook.fr>
2023-04-10 19:59:32 +0200

    add animation on film

--- js/film.js
+++ js/film.js
@@ -1,10 +1,21 @@
 
 
 gsap.to(".mc75", {x : 325, y : -250 , duration :  10 , scale: 3.5})
-gsap.to(".mc75" , {y : -245  , duration : 3 , delay : 10 , repeat :-1 , repeatDelay : 2})
+gsap.to(".mc75" , {y : -240  , duration : 3 , delay : 10 , repeat :-1 , repeatDelay : 2})
 gsap.to(".mc75" ,   { y : -250  , duration : 2 , delay : 11 , repeat :-1 , repeatDelay : 3 })
 
 
-gsap.to(".destroyer", {x : -325, y : 250 , duration :  10 , scale: 3})
-gsap.to(".destroyer" , {y : 245  , duration : 3 , delay : 10 , repeat :-1 , repeatDelay : 2})
-gsap.to(".destroyer" ,   { y : 250  , duration : 2 , delay : 11 , repeat :-1 , repeatDelay : 3 })
+gsap.to(".destroyer", {x : -325, y : 300 , duration :  10 , scale: 3})
+gsap.to(".destroyer" , {y : 290  , duration : 3 , delay : 10 , repeat :-1 , repeatDelay : 2})
+gsap.to(".destroyer" ,   { y : 300  , duration : 2 , delay : 11 , repeat :-1 , repeatDelay : 3 })
+
+
+gsap.to(".destroyer1", {x : -295, y : 200 , duration :  10 , scale: 2})
+gsap.to(".destroyer1" , {y : 190  , duration : 3 , delay : 10 , repeat :-1 , repeatDelay : 2})
+gsap.to(".destroyer1" ,   { y : 200  , duration : 2 , delay : 11 , repeat :-1 , repeatDelay : 3 })
+
+
+
+gsap.to(".destroyer2", {x : -295, y : 380 , duration :  10 , scale: 2.5})
+gsap.to(".destroyer2" , {y : 370  , duration : 3 , delay : 10 , repeat :-1 , repeatDelay : 2})
+gsap.to(".destroyer2" ,   { y : 380  , duration : 2 , delay : 11 , repeat :-1 , repeatDelay : 3 })
