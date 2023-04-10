2cc41960121a6d6f0835fcefcf18ddd06a780401
Nalithdev <Ethan.delcroix@outlook.fr>
2023-04-10 20:52:34 +0200

    add animation on Xwing and TIE

--- js/film.js
+++ js/film.js
@@ -1,5 +1,4 @@
 
-
 gsap.to(".mc75", {x : 325, y : -250 , duration :  10 , scale: 3.5})
 gsap.to(".mc75" , {y : -240  , duration : 3 , delay : 10 , repeat :-1 , repeatDelay : 2})
 gsap.to(".mc75" ,   { y : -250  , duration : 2 , delay : 11 , repeat :-1 , repeatDelay : 3 })
@@ -19,3 +18,46 @@ gsap.to(".destroyer1" ,   { y : 200  , duration : 2 , delay : 11 , repeat :-1 , 
 gsap.to(".destroyer2", {x : -295, y : 380 , duration :  10 , scale: 2.5})
 gsap.to(".destroyer2" , {y : 370  , duration : 3 , delay : 10 , repeat :-1 , repeatDelay : 2})
 gsap.to(".destroyer2" ,   { y : 380  , duration : 2 , delay : 11 , repeat :-1 , repeatDelay : 3 })
+
+
+
+gsap.to('.t1', {opacity : 1 , duration : 1 , delay : 10})
+gsap.to('.t2', {opacity : 1 , duration : 1 , delay : 10})
+gsap.to('.t3', {opacity : 1 , duration : 1 , delay : 10})
+gsap.to('.t4', {opacity : 1 , duration : 1  , delay : 10})
+gsap.to('.xwing', {opacity : 1 , duration : 1 , delay : 10})
+
+gsap.to('.t1', {y : 300 , duration : 2 , delay : 10})
+gsap.to('.t2', {y : 300 , duration : 2 , delay : 10})
+gsap.to('.t3', {y : 300 , duration : 2 , delay : 10})
+gsap.to('.t4', {y : 290 , duration : 2 , delay : 10})
+gsap.to('.xwing', {y : 200 , duration : 2 , delay : 10})
+
+gsap.to('.t1', {x : -125 , duration : 2 , delay : 11.5})
+gsap.to('.t2', {x : -50 , duration : 2 , delay : 11.5})
+gsap.to('.t3', {x : -75 , duration : 2 , delay : 11.5})
+gsap.to('.t4', {x : -150 , duration : 2 , delay : 11.5})
+gsap.to('.xwing', {x : 300  , duration : 2 , delay : 11 , rotate : -50})
+gsap.to('.xwing' , {y : -50, duration : 2 , delay : 11.5 , rotate : 90})
+
+
+gsap.to('.xwing', {x : 1250 , duration : 2.5 , delay : 12.5})
+gsap.to('.t1' , {y : 275 , duration : 1 , delay : 13 })
+gsap.to('.t2', {y : 350 , duration : 2 , delay : 13})
+gsap.to('.t3', {y : 350 , duration : 2 , delay : 13})
+gsap.to('.t4', {y : 390 , duration : 2 , delay : 13})
+
+
+gsap.to(".xwing" , {rotate : -90 , delay : 15 , duration : 1})
+gsap.to(".xwing" , {x: 450 , delay : 16 , duration : 3})
+gsap.to('.xwing' , {rotate : 90 , duration : 2 , delay : 19})
+
+gsap.to('.t1', {y : 300 , duration : 2 , delay : 16 })
+gsap.to('.t2', {y : 300 , duration : 2 , delay : 16})
+gsap.to('.t3', {y : 300 , duration : 2 , delay : 16})
+gsap.to('.t4', {y : 290 , duration : 2 , delay : 16})
+
+gsap.to('.t1', {x : -225 , duration : 2 , delay : 19 })
+gsap.to('.t2', {x : -150 , duration : 2 , delay : 19})
+gsap.to('.t3', {x : -175 , duration : 2 , delay : 19})
+gsap.to('.t4', {x : -250 , duration : 2 , delay : 19})
